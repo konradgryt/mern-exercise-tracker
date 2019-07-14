85a817dc5a552db2b9b844c5222dfe86e9c975b0
konradgryt <konrad.gryt@gmail.com>
2019-07-14 12:33:31 +0200

    Add WIP components: ExercisesList, EditExercise, CreateExercise and CreateUser

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component"
-// import ExercisesList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercise.component";
-// import CreateExercise from "./components/create-exercise.component";
-// import CreateUser from "./components/create-user.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
-      {/* <Route path="/" exact component={ExercisesList} />
+      <Route path="/" exact component={ExercisesList} />
       <Route path="/edit:id" exact component={EditExercise} />
       <Route path="/create" exact component={CreateExercise} />
-      <Route path="/user" exact component={CreateUser} /> */}
-      <div className="container">
-        Hello world
-      </div>
+      <Route path="/user" exact component={CreateUser} />
     </Router>
   );
 }
