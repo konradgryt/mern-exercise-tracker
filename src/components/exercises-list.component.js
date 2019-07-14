7e7f6605d0c8f8cc3ed982d06bdb704d32e54b9b
konradgryt <konrad.gryt@gmail.com>
2019-07-14 22:01:30 +0200

    Add edit exercises component, fix route for editing

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+//functional component - lack of states and lifecycle methods 
 const Exercise = props => (
   <tr>
     <td>{props.exercise.username}</td>
