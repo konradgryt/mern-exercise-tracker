e7e9eacaa3902f48faa3a8662a335f8eedcbbaab
konradgryt <konrad.gryt@gmail.com>
2019-07-14 13:26:19 +0200

    Add duration input, description input and datepicker to the form

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,6 @@
 import React, { Component} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"
 
 export default class CreateExercise extends Component {
   
@@ -62,7 +64,7 @@ export default class CreateExercise extends Component {
       date: this.state.date,
     }
 
-    console.table(exercise);
+    console.log(exercise);
 
     window.location = '/';
   }
@@ -78,6 +80,7 @@ export default class CreateExercise extends Component {
             <select 
               ref="userInput"
               required
+              readOnly
               className="form-control"
               value={this.state.username}
               onChange={this.onChangeUsername}>
@@ -87,6 +90,20 @@ export default class CreateExercise extends Component {
                 })
               }
             </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input type="text" required className="form-control" value={this.state.description} onChange={this.onChangeDescription} />
+          </div>
+          <div className="form-group">
+            <label>Duration: </label>
+            <input type="text" required className="form-control" value={this.state.duration} onChange={this.onChangeDuration} />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker selected={this.state.date} onChange={this.onChangeDate} />
+            </div>
           </div>
           <div className="form-group">
             <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
